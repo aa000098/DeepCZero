fa527dbb6d731fafb88f12a8e88c517878cdb944
hyunson <aa000098@naver.com>
2015-09-25 16:57:32 +0900

    feat: VGG16 layer implemented

--- src/container/layer/model.cpp
+++ src/container/layer/model.cpp
@@ -1,5 +1,6 @@
 #include "container/layer/model.hpp"
 #include "graph/utils/utils.hpp"
+#include "utils/io.hpp"
 
 void Model::plot(std::vector<Variable> inputs,
 				std::string to_file) {
@@ -29,4 +30,57 @@ Variable MLP::forward(const std::vector<Variable>& xs) {
 		x = (*activation)(x);
 	}
 	return (*layers.back())(x);
+}
+
+VGG16::VGG16(bool pretrained) {
+	std::pair<size_t, size_t> k = {3, 3};
+	std::pair<size_t, size_t> s = {1, 1};
+	std::pair<size_t, size_t> p = {1, 1};
+	conv1_1 = std::make_shared<Conv2d>(64, k, s, p);
+	conv1_2 = std::make_shared<Conv2d>(64, k, s, p);
+	conv2_1 = std::make_shared<Conv2d>(128, k, s, p);
+	conv2_2 = std::make_shared<Conv2d>(128, k, s, p);
+	conv3_1 = std::make_shared<Conv2d>(256, k, s, p);
+	conv3_2 = std::make_shared<Conv2d>(256, k, s, p);
+	conv3_3 = std::make_shared<Conv2d>(256, k, s, p);
+	conv4_1 = std::make_shared<Conv2d>(512, k, s, p);
+	conv4_2 = std::make_shared<Conv2d>(512, k, s, p);
+	conv4_3 = std::make_shared<Conv2d>(512, k, s, p);
+	conv5_1 = std::make_shared<Conv2d>(512, k, s, p);
+	conv5_2 = std::make_shared<Conv2d>(512, k, s, p);
+	conv5_3 = std::make_shared<Conv2d>(512, k, s, p);
+	fc6 = std::make_shared<Linear>(4096);
+	fc7 = std::make_shared<Linear>(4096);
+	fc8 = std::make_shared<Linear>(1000);
+
+	if (pretrained) {
+		std::string weights_path = get_file(this->WEIGHTS_PATH);
+	}
+}
+
+Variable VGG16::forward(const std::vector<Variable>& xs) {
+	Variable x = xs[0];
+	x =  relu((*conv1_1)(x));
+	x =  relu((*conv1_2)(x));
+	x = pooling(x, {2, 2}, {2, 2});
+	x =  relu((*conv2_1)(x));
+	x =  relu((*conv2_2)(x));
+	x = pooling(x, {2, 2}, {2, 2});
+	x =  relu((*conv3_1)(x));
+	x =  relu((*conv3_2)(x));
+	x =  relu((*conv3_3)(x));
+	x = pooling(x, {2, 2}, {2, 2});
+	x =  relu((*conv4_1)(x));
+	x =  relu((*conv4_2)(x));
+	x =  relu((*conv4_3)(x));
+	x = pooling(x, {2, 2}, {2, 2});
+	x =  relu((*conv5_1)(x));
+	x =  relu((*conv5_2)(x));
+	x =  relu((*conv5_3)(x));
+	x = pooling(x, {2, 2}, {2, 2});
+	x = reshape(x, {x.shape()[0], x.shape()[3]});
+	x = dropout(relu((*fc6)(x)));
+	x = dropout(relu((*fc7)(x)));
+	x = (*fc8)(x);
+	return x;
 }

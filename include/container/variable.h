7edab185138cdddafe5877019b7ced727ad08538
aa000098 <aa000098@naver.com>
2025-04-12 00:22:44 +0900

    fix: fix function.h

--- include/container/variable.h
+++ include/container/variable.h
@@ -1,5 +1,21 @@
+#pragma once
+
 #include <memory>
-#include <string>
+
+class Function; // forward declaration
 
 class Variable {
+
+    float data;
+    float grad;
+    std::shared_ptr<Function> creator;
+    bool requires_grad;
+
+public:
+    Variable(float data, bool requires_grad = true);
+
+    void set_creator(std::shared_ptr<Function> func);
+    void backward();
+    void show() const;
 };
+	

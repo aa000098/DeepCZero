d23945e577b06b8c7f779d9f2ca88dfa72419e27
hyunson <aa000098@naver.com>
2025-05-05 00:06:13 +0900

    fix: my_sin test code works well

--- test/function/my_sin_test.cpp
+++ test/function/my_sin_test.cpp
@@ -7,12 +7,16 @@ Variable my_sin(Variable x, float threshold=0.0001) {
 	Variable y({0});
 	
 	for (size_t i = 0; i < 100000; ++i) {
-		float maclaurin = static_cast<float>(std::pow(-1, i) / std::tgamma(2*i+1));
+		float maclaurin = static_cast<float>(std::pow(-1, i) / std::tgamma(2*i+2));
 		Variable c({maclaurin});
 		Variable t = c * (x^(2*i+1));		
+		y.set_name("y" + std::to_string(i));
 		y = y + t;
+	/*	
+		std::cout << "[" << i << "] : ";
+		std::cout << std::abs(t.data().raw_data()[0]) << "*" << maclaurin << std::endl;
 		y.show();
-		std::cout << std::abs(t.data().raw_data()[0]) << std::endl;
+	*/
 		if (std::abs(t.data().raw_data()[0]) < threshold)
 			break;
 	}

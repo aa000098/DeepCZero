cd3a849d828db541e37a2425a918d0795e1009db
hyunson <aa000098@naver.com>
2025-05-25 01:47:09 +0900

    feat: linear regression test added

--- test/linear/linear_regression_test.cpp
+++ test/linear/linear_regression_test.cpp
@@ -2,12 +2,12 @@
 #include <random>
 
 
-Tensor<> rand_tensor(size_t rows, size_t cols) {
+Tensor<> rand_tensor(size_t rows, size_t cols, size_t seed = 42) {
 	//std::random_device rd;
 	//std::mt19937 gen(rd());
-	std::mt19937 gen(42);
+	std::mt19937 gen(seed);
 
-	std::uniform_real_distribution<> dist(0, 10);
+	std::uniform_real_distribution<> dist(-10, 10);
 
 	std::vector<float> data;
 	data.reserve(rows * cols);
@@ -23,22 +23,18 @@ Variable predict(Variable& x, Variable& W, Variable& b) {
 
 Variable mean_squared_error(Variable& x0, Variable& x1) {
 	Variable diff = x0 - x1;
-	sum(diff^2).show();
 	return sum(diff^2) / diff.size();
 }
 
 int main() {
-	//Tensor x_rand = rand_tensor(100, 1);
-	//Tensor y_rand = 5.0f + 2.0f * x_rand + rand_tensor(100, 1);
+	Tensor x_rand = rand_tensor(100, 1);
+	Tensor y_rand = 5.0f + 2.0f * x_rand + rand_tensor(100, 1, 23);
 
-	Tensor x_rand({3,1}, {1,2,3});
-	Tensor y_rand = 5.0f + 2.0f * x_rand + Tensor({3,1}, {1,2,3});
+//	Tensor x_rand({3,1}, {1,2,3});
+//	Tensor y_rand = 5.0f + 2.0f * x_rand + Tensor({3,1}, {1,2,3});
 
 	Variable x(x_rand);
 	Variable y(y_rand);
-
-	x.show();
-	y.show();
 
 	Tensor w_tensor({1,1});
 	Tensor b_tensor({1});
@@ -48,41 +44,25 @@ int main() {
 	W.set_name("W");
 	b.set_name("b");
 
-	float lr = 0.1;
-	size_t iters = 100;
+	float lr = 0.01;
+	size_t iters = 1000;
 
 	Variable loss;
-	W.show();
-	b.show();
+	
 
 	for (size_t i = 0; i < iters; i++) {
 		Variable y_pred = predict(x, W, b);
 		loss = mean_squared_error(y, y_pred);
-		y_pred.set_name("y_pred");	
-		W.set_name("W1");
-		b.set_name("b1");
-		loss.set_name("loss1");
-
-		y_pred.show();
-		W.show();
-		b.show();
-		loss.show();
 
 		W.cleargrad();
 		b.cleargrad();
 		loss.backward();
-		W.set_name("W2");
-		b.set_name("b2");
-		loss.set_name("loss2");
-
-		W.show();
-		b.show();
 
 		W.data() -= lr * W.grad().data();
 		b.data() -= lr * b.grad().data();
-		W.set_name("W3");
-		b.set_name("b3");
-		loss.set_name("loss3");
+		W.set_name("W");
+		b.set_name("b");
+		loss.set_name("loss");
 	
 		W.show();
 		b.show();

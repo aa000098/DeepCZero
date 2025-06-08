a962ec92fd4acffc7cdf534e5cebe09ac318f814
hyunson <aa000098@naver.com>
2025-06-08 22:25:04 +0900

    feat: softmax function added

--- include/function/activation_functions.hpp
+++ include/function/activation_functions.hpp
@@ -11,4 +11,15 @@ public:
 	~Sigmoid() = default;
 };
 
+class Softmax : public Function {
+private:
+	std::vector<int> axis;
+
+public:
+	Softmax(std::vector<int> axis) : axis(axis) {};
+	Variable forward(const std::vector<Variable>& xs) override;
+	std::vector<Variable> backward(const Variable& gy) override;
+	~Softmax() = default;
+};
+
 }
--- src/function/activation_functions.cpp
+++ src/function/activation_functions.cpp
@@ -13,3 +13,22 @@ std::vector<Variable> function::Sigmoid::backward(const Variable& gy) {
 	Variable gx = gy * y * (1.0f - y);
 	return { gx };
 }
+
+Variable function::Softmax::forward(const std::vector<Variable>& xs) {
+	const Tensor<>& x = xs[0].data();
+
+	Tensor y = x.max(axis, true);
+	y = exp(y);
+	Tensor<> sum_y = y.sum(axis, true);
+	return y / sum_y;
+}
+
+std::vector<Variable> function::Softmax::backward(const Variable& gy) {
+	Variable y = output.lock();
+
+	Variable gx = y * gy;
+	Variable sumdx = sum(gx, axis, true);
+	gx -= y * sumdx;
+	return { gx };
+
+}

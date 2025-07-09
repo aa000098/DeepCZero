4c9ffed5b25c1296e9610f23b1e9bf9e4051977b
hyunson <aa000098@naver.com>
2025-07-09 22:50:07 +0900

    fix: conv_function fixed

--- src/function/conv_functions.cpp
+++ src/function/conv_functions.cpp
@@ -3,11 +3,12 @@
 
 Variable function::Im2col::forward(const std::vector<Variable>& xs) {
 	const Tensor<>& x = xs[0].data();
-
+	std::vector<size_t> x_shape = x.get_shape();
+	
 	return Variable(x);
 }
 
 std::vector<Variable> function::Im2col::backward(const Variable& gy) {
 	const Variable& x = inputs[0];
-	return {x};
+	return {gy * x};
 }

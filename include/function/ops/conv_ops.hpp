4c79c572fe520e1b3b96c99c92b14b5213d7aca6
hyunson <aa000098@naver.com>
2025-07-20 10:40:37 +0900

    feat: conv2d and deconv2d ops added

--- include/function/ops/conv_ops.hpp
+++ include/function/ops/conv_ops.hpp
@@ -5,6 +5,21 @@
 
 class Variable;
 
+Variable conv2d(const Variable& x,
+				const Variable& W,
+				const Variable& b,
+				std::pair<size_t, size_t> stride,
+				std::pair<size_t, size_t> pad);
+
+Variable deconv2d(const Variable& x,
+				const Variable& W,
+				const Variable& b,
+				std::pair<size_t, size_t> stride,
+				std::pair<size_t, size_t> pad,
+				std::pair<size_t, size_t> out_size);
+
+
+
 Variable im2col(const Variable& x,
 				std::pair<size_t, size_t> kernel_size,
 				std::pair<size_t, size_t> stride,

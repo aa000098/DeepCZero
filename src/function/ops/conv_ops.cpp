f1c8e04b7f466fa3341ba80faca504df1b1b0087
hyunson <aa000098@naver.com>
2025-07-25 23:07:08 +0900

    feat: conv2d, deconv2d, conv2dgradw ops added

--- src/function/ops/conv_ops.cpp
+++ src/function/ops/conv_ops.cpp
@@ -3,6 +3,44 @@
 
 #include <memory>
 
+
+// [Convolution]
+
+Variable conv2d(const Variable &x,
+				const Variable &W,
+				const Variable &b,
+				std::pair<size_t, size_t> stride={1,1},
+				std::pair<size_t, size_t> pad={0,0}) {
+	using namespace function;
+	std::shared_ptr<Function> f = std::make_shared<Conv2d>(stride, pad);
+	return (*f)({x, W, b});
+}
+				
+Variable deconv2d(const Variable &x,
+				const Variable &W,
+				const Variable &b,
+				std::pair<size_t, size_t> stride={1,1},
+				std::pair<size_t, size_t> pad={0,0},
+				std::pair<size_t, size_t> outsize={0,0}) {
+	using namespace function;
+	std::shared_ptr<Function> f = std::make_shared<Deconv2d>(stride, pad, outsize);
+	return (*f)({x, W, b});
+}
+
+Variable conv2dgradw(const Variable &x,
+					const Variable &gy,
+					const Variable &W,
+					std::pair<size_t, size_t> stride={1,1},
+					std::pair<size_t, size_t> pad={0,0}) {
+	using namespace function;
+	std::shared_ptr<Function> f = std::make_shared<Conv2dGradW>(W, stride, pad);
+	return (*f)({x, gy});
+}
+				
+
+
+// [Im2col]
+
 Variable im2col(const Variable& x,
 				std::pair<size_t, size_t> kernel_size,
 				std::pair<size_t, size_t> stride,

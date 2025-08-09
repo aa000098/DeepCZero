98bf82e144bfc52071c815827f3f77919ad13cf3
hyunson <aa000098@naver.com>
2025-08-09 22:24:52 +0900

    fix: tensor function reference fixed

--- include/container/tensor/tensor_functions.hpp
+++ include/container/tensor/tensor_functions.hpp
@@ -41,14 +41,14 @@ Tensor<DstT> cast_tensor(const Tensor<SrcT>& src);
 
 
 template<typename T>
-Tensor<T> im2col_array(	const Tensor<T> img,
+Tensor<T> im2col_array(	const Tensor<T> &img,
 						std::pair<size_t, size_t> kernel_size,
 						std::pair<size_t, size_t> stride,
 						std::pair<size_t, size_t> pad,
 						bool to_matrix=true);
 
 template<typename T>
-Tensor<T> col2im_array(	const Tensor<T> col,
+Tensor<T> col2im_array(	const Tensor<T> &col,
 						std::vector<size_t> img_shape,
 						std::pair<size_t, size_t> kernel_size,
 						std::pair<size_t, size_t> stride,

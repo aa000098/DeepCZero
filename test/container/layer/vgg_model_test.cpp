4ddb2b1b7c76080525790d50589e7e7fd9c53189
hyunson <aa000098@naver.com>
2025-09-17 10:00:00 +0900

    feat: vgg test added

---
+++ test/container/layer/vgg_model_test.cpp
@@ -0,0 +1,12 @@
+#include "deepczero.hpp"
+
+void test_vgg_forward_backward() {
+	Tensor<> x_data({1, 3, 244, 244}, 1);
+	Variable x(x_data);
+	VGG16 model;	
+	Variable y = model.forward({x});
+}
+
+int main() {
+	test_vgg_forward_backward();
+}
